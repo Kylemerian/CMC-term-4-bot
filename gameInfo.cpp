d3e9cee0d3d83c9ea70f4bc34a2b03bd80ca3e49
Kylemerian <lawerenceses@mail.ru>
2021-03-16 21:47:25 +0300

    minor_changes

--- gameInfo.cpp
+++ gameInfo.cpp
@@ -72,9 +72,10 @@ void gameInfo::print() const
     printf("nick       raw prod  money plants  num  Bought num Sold\n");
     for (int i = 0; i < players; i++){
         if(users[i].isAlive){
-            printf("%10s %3d %4d %6d %6d %4d %7d %3d %4d\n", users[i].nick, users[i].raw,
-                users[i].prod, users[i].money, users[i].factories, users[i].results.numBought,
-                    users[i].results.bought, users[i].results.numSold, users[i].results.sold);
+            printf("%10s %3d %4d %6d %6d %4d %7d %3d %4d\n", users[i].nick, 
+                users[i].raw, users[i].prod, users[i].money,users[i].factories,
+                users[i].results.numBought,users[i].results.bought,
+                users[i].results.numSold, users[i].results.sold);
         }
     }
     printf("BUY PRICE = %d\nSELL PRICE = %d\n", prices.bought, prices.sold);
@@ -146,24 +147,24 @@ void gameInfo::setTradeResults(int servfd, textline & cmd)
     }
     
     while(!cmd.hasSubStr("ENDTURN")){
-            cmd.getNext(servfd);
-            if(cmd.isEnded())
-                break;
-            if(cmd.hasSubStr("BOUGHT")){
-                int tmp;
-                for (int i = 0; i < players; i++)
-                    if(cmd.hasSubStr(users[i].nick))
-                        tmp = i;
-                cmd.cleanAllChars();
-                cmd.getTwoNum(users[tmp].results.numBought, users[tmp].results.bought);
-            }
-            if(cmd.hasSubStr("SOLD")){
-                int tmp;
-                for (int i = 0; i < players; i++)
-                    if(cmd.hasSubStr(users[i].nick))
-                        tmp = i;
-                cmd.cleanAllChars();
-                cmd.getTwoNum(users[tmp].results.numSold, users[tmp].results.sold);
-            }
+        cmd.getNext(servfd);
+        if(cmd.isEnded())
+            break;
+        if(cmd.hasSubStr("BOUGHT")){
+            int tmp;
+            for (int i = 0; i < players; i++)
+                if(cmd.hasSubStr(users[i].nick))
+                    tmp = i;
+            cmd.cleanAllChars();
+            cmd.getTwoNum(users[tmp].results.numBought, users[tmp].results.bought);
+        }
+        if(cmd.hasSubStr("SOLD")){
+            int tmp;
+            for (int i = 0; i < players; i++)
+                if(cmd.hasSubStr(users[i].nick))
+                    tmp = i;
+            cmd.cleanAllChars();
+            cmd.getTwoNum(users[tmp].results.numSold, users[tmp].results.sold);
+        }
     }
 }
